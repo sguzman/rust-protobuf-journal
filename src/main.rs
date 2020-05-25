91ac082c6ffa08b5287e83cf8b64bb0e0ca62c4a
Salvador Guzman <guzmansalv@gmail.com>
2020-05-25 10:08:00 -0700

    workign code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+extern crate chrono;
 extern crate quick_protobuf;
 
 pub mod journal;
 
+use chrono::prelude::*;
 use std::env;
 use journal::JournalEntry;
+use std::borrow::Cow;
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let args: String = args.join(" ");
-    println!("{}", args);
+    let journal: JournalEntry = {
+        let message: Cow<str> = {
+            let message: Vec<String> = env::args().skip(1).collect();
+            let message: String = message.join(" ");
+
+            Cow::from(message)
+        };
+        let time: u32 = 3;
+
+        JournalEntry {
+            time,
+            message
+        }
+    };
+
+    println!("{:?}", journal);
 }
