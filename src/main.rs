34de855ad279d6c9315ae874e91a97036a96d609
Salvador Guzman <guzmansalv@gmail.com>
2020-05-25 09:36:05 -0700

    working cmd arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate quick_protobuf;
+
+pub mod journal;
+
+use std::env;
+use journal::JournalEntry;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    let args: String = args.join(" ");
+    println!("{}", args);
 }
